4f62ceb3a1806f01d05861ddae9ced7acd7f5b94
devensh <devensh@163.com>
2020-12-26 00:51:19 -0800

    手机号重复注册

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -9,7 +9,6 @@ from meiduo_mall.utils.response_code import RETCODE
 import re
 
 
-# Create your views here.
 class UsernameCountView(View):
     """判断用户名是否重复注册"""
 
@@ -20,6 +19,19 @@ class UsernameCountView(View):
         :return: JSON
         """
         count = User.objects.filter(username=username).count()
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'count': count})
+
+
+class MobileCountView(View):
+    """判断手机号是否重复注册"""
+
+    def get(self, request, mobile):
+        """
+        :param request: 请求对象
+        :param mobile: 手机号
+        :return: JSON
+        """
+        count = User.objects.filter(mobile=mobile).count()
         return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'count': count})
 
 
@@ -58,7 +70,6 @@ class RegisterView(View):
         if allow != 'on':
             return http.HttpResponseForbidden('请勾选用户协议')
         # 保存注册数据
-        print("---1---", username, password, mobile)
         try:
             user = User.objects.create_user(username=username, password=password, mobile=mobile)
         except DatabaseError:

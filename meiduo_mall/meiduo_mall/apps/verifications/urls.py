751234fd469c65295fa8270920fd4a3ac917dcaf
devensh <devensh@163.com>
2021-01-08 21:59:07 -0800

    短信验证码-后端逻辑

--- meiduo_mall/meiduo_mall/apps/verifications/urls.py
+++ meiduo_mall/meiduo_mall/apps/verifications/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^image_codes/(?P<uuid>[\w-]+)/$', views.ImageCodeView.as_view()),  # 图形验证码
+    url(r'sms_codes/(?P<mobile>1[3-9]\d{9})/$', views.SMSCodeView.as_view()),  # 短信验证码
 ]

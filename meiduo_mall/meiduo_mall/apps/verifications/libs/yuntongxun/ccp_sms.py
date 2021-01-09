2110b309826acf356fe1757bf5a2d4d1a59e603c
devensh <devensh@163.com>
2021-01-08 19:32:51 -0800

    短信验证码-封装发送短信单例类

--- meiduo_mall/meiduo_mall/apps/verifications/libs/yuntongxun/ccp_sms.py
+++ meiduo_mall/meiduo_mall/apps/verifications/libs/yuntongxun/ccp_sms.py
@@ -28,16 +28,45 @@ _softVersion = '2013-12-26'
 # @param to 手机号码
 # @param datas 内容数据 格式为数组 例如：{'12','34'}，如不需替换请填 ''
 # @param $tempId 模板Id
-def sendTemplateSMS(to, datas, tempId):
-    # 初始化REST SDK
-    rest = REST(_serverIP, _serverPort, _softVersion)
-    rest.setAccount(_accountSid, _accountToken)
-    rest.setAppId(_appId)
+# def sendTemplateSMS(to, datas, tempId):
+#     # 初始化REST SDK
+#     rest = REST(_serverIP, _serverPort, _softVersion)
+#     rest.setAccount(_accountSid, _accountToken)
+#     rest.setAppId(_appId)
+#
+#     result = rest.sendTemplateSMS(to, datas, tempId)
+#     print(result)
 
-    result = rest.sendTemplateSMS(to, datas, tempId)
-    print(result)
+
+class CCP(object):
+    """发送短信的单例类"""
+
+    def __new__(cls, *args, **kwargs):
+        # 判断是否存在类属性_instance，_instance是类CCP的唯一对象，即单例
+        if not hasattr(CCP, "_instance"):
+            cls._instance = super(CCP, cls).__new__(cls, *args, **kwargs)
+            cls._instance.rest = REST(_serverIP, _serverPort, _softVersion)
+            cls._instance.rest.setAccount(_accountSid, _accountToken)
+            cls._instance.rest.setAppId(_appId)
+        return cls._instance
+
+    def send_template_sms(self, to, datas, temp_id):
+        """
+        发送模板短信单例方法
+        :param to: 注册手机号
+        :param datas: 模板短信内容数据，格式为列表，例如：['123456', 5]，如不需替换请填 ''
+        :param temp_id: 模板编号，默认免费提供id为1的模板
+        :return: 发短信结果
+        """
+        result = self.rest.sendTemplateSMS(to, datas, temp_id)
+        if result.get("statusCode") == "000000":
+            # 返回0，表示发送短信成功
+            return 0
+        else:
+            # 返回-1，表示发送失败
+            return -1
 
 
 if __name__ == '__main__':
     # 注意： 测试的短信模板编号为1
-    sendTemplateSMS('18100209298', ['123456', 5], 1)
+    CCP().send_template_sms('18100209298', ['123456', 5], 1)

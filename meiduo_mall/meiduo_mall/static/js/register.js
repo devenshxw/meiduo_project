14108b16d247c5ffd6df232661ed82a631e7ba22
devensh <devensh@163.com>
2021-01-09 01:02:00 -0800

    短信验证码-前端逻辑

--- meiduo_mall/meiduo_mall/static/js/register.js
+++ meiduo_mall/meiduo_mall/static/js/register.js
@@ -10,6 +10,9 @@ let vm = new Vue({
         allow: '',
         image_code_url: '',
         image_code: '',
+        sms_code: '',
+        sms_code_tip: '获取短信验证码',
+        send_flag: false,
 
         error_name: false,
         error_password: false,
@@ -17,16 +20,72 @@ let vm = new Vue({
         error_mobile: false,
         error_allow: false,
         error_image_code: false,
+        error_sms_code: false,
 
         error_name_message: '',
         error_mobile_message: '',
         error_image_code_message: '',
+        error_sms_code_message: '',
     },
     mounted(){
         // 生成图形验证码
         this.generate_image_code();
     },
     methods: {
+        // 发送短信验证码
+        send_sms_code(){
+            // 避免重复点击
+            if (this.sending_flag == true) {
+                return;
+            }
+            this.sending_flag = true;
+
+            // 校验参数
+            this.check_mobile();
+            this.check_image_code();
+            if (this.error_mobile == true || this.error_image_code == true) {
+                this.sending_flag = false;
+                return;
+            }
+
+            // 请求短信验证码
+            let url = '/sms_codes/' + this.mobile + '/?image_code=' + this.image_code+'&uuid='+ this.uuid;
+            axios.get(url, {
+                responseType: 'json'
+            })
+                .then(response => {
+                    if (response.data.code == '0') {
+                        // 倒计时60秒
+                        var num = 60;
+                        var t = setInterval(() => {
+                            if (num == 1) {
+                                clearInterval(t);
+                                this.sms_code_tip = '获取短信验证码';
+                                this.generate_image_code();
+                                this.sending_flag = false;
+                            } else {
+                                num -= 1;
+                                // 展示倒计时信息
+                                this.sms_code_tip = num + '秒';
+                            }
+                        }, 1000, 60)
+                    } else {
+                        if (response.data.code == '4001') {
+                            this.error_image_code_message = response.data.errmsg;
+                            this.error_image_code = true;
+                        } else { // 4002
+                            this.error_sms_code_message = response.data.errmsg;
+                            this.error_sms_code = true;
+                        }
+                        this.generate_image_code();
+                        this.sending_flag = false;
+                    }
+                })
+                .catch(error => {
+                    console.log(error.response);
+                    this.sending_flag = false;
+                })
+        },
         // 生成图形验证码
         generate_image_code(){
             // 生成UUID。generateUUID() : 封装在common.js文件中，需要提前引入
@@ -114,6 +173,15 @@ let vm = new Vue({
                 this.error_image_code = true;
             } else {
                 this.error_image_code = false;
+            }
+        },
+        // 校验短信验证码
+        check_sms_code(){
+            if(this.sms_code.length != 6){
+                this.error_sms_code_message = '请填写短信验证码';
+                this.error_sms_code = true;
+            } else {
+                this.error_sms_code = false;
             }
         },
         // 校验是否勾选协议

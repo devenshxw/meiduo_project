a434dfedc8453ceb8fa9297bea58331265bf6540
devensh <devensh@163.com>
2021-01-20 06:05:35 -0800

    用户中心-收货地址-展示收货地址界面

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     url(r'^info/$', views.UserInfoView.as_view(), name='info'),  # 用户中心
     url(r'^emails/$', views.EmailView.as_view()),  # 添加邮箱
     url(r'^emails/verification/$', views.VerifyEmailView.as_view()),  # 验证邮箱
+    url(r'^addresses/$', views.AddressView.as_view(), name='address'),  # 收货地址
 ]

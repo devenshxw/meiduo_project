3dfbfb61ba0ec1f666756877175810a9bac72748
devensh <devensh@163.com>
2021-01-20 08:09:51 -0800

    用户中心-省市区三级联动-查询市区数据

--- meiduo_mall/meiduo_mall/apps/areas/views.py
+++ meiduo_mall/meiduo_mall/apps/areas/views.py
@@ -33,5 +33,24 @@ class AreasView(View):
             return JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'province_list': province_list})
         else:
             # 提供市或区数据
-            pass
+            try:
+                parent_model = Area.objects.get(id=area_id)  # 查询市或区的父级
+                sub_model_list = parent_model.subs.all()
+
+                # 序列化市或区数据
+                sub_list = []
+                for sub_model in sub_model_list:
+                    sub_list.append({'id': sub_model.id, 'name': sub_model.name})
+
+                sub_data = {
+                    'id': parent_model.id,  # 父级pk
+                    'name': parent_model.name,  # 父级name
+                    'subs': sub_list  # 父级的子集
+                }
+            except Exception as e:
+                logger.error(e)
+                return JsonResponse({'code': RETCODE.DBERR, 'errmsg': '城市或区数据错误'})
+
+                # 响应市或区数据
+            return JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK', 'sub_data': sub_data})
 

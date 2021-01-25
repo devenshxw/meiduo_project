fb4835468934f68f50ba19d0cd4f5b6dbf4b288c
devensh <devensh@163.com>
2021-01-25 19:49:21 +0800

    商品-首页广告-展示首页商品广告

--- meiduo_mall/meiduo_mall/apps/contents/views.py
+++ meiduo_mall/meiduo_mall/apps/contents/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from collections import OrderedDict
 from goods.models import GoodsChannel
+from contents.models import ContentCategory
 
 
 # Create your views here.
@@ -34,8 +35,15 @@ class IndexView(View):
                     cat2.sub_cats.append(cat3)
                 categories[group_id]['sub_cats'].append(cat2)
 
+        # 广告数据
+        contents = {}
+        content_categories = ContentCategory.objects.all()
+        for cat in content_categories:
+            contents[cat.key] = cat.content_set.filter(status=True).order_by('sequence')
+
         # 渲染模板的上下文
         context = {
             'categories': categories,
+            'contents': contents,
         }
         return render(request, 'index.html', context)

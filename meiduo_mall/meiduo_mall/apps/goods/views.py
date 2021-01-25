47e59f9b022b087cd2352ea6c28847fe2da12584
devensh <devensh@163.com>
2021-01-25 21:56:55 +0800

    商品-商品列表页-查询商品频道分类

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.views import View
+from django import http
 
-# Create your views here.
+from goods.models import GoodsCategory
+from contents.utils import get_categories
+
+
 class ListView(View):
     """商品列表页"""
 
     def get(self, request, category_id, page_num):
         """提供商品列表页"""
-        return render(request, 'list.html')
+        # 判断category_id是否正确
+        try:
+            category = GoodsCategory.objects.get(id=category_id)
+        except GoodsCategory.DoesNotExist:
+            return http.HttpResponseNotFound('GoodsCategory does not exist')
+
+        # 查询商品频道分类
+        categories = get_categories()
+
+        # 渲染页面
+        context = {
+            'categories':categories,
+        }
+        return render(request, 'list.html', context)

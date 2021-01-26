9a32b155b116ce4c8183da96ddfdc81a0f37ec63
devensh <devensh@163.com>
2021-01-26 21:35:53 +0800

    商品-商品列表页-列表页分页和排序-查询列表页分页和排序数据+修正SKU模型类的default_image字段类型为ImageField

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 from django import http
+from django.core.paginator import Paginator, EmptyPage
 
-from goods.models import GoodsCategory
+from goods.models import GoodsCategory, SKU
 from contents.utils import get_categories
 from goods.utils import get_breadcrumb
+from . import constants
 
 
 class ListView(View):
@@ -17,15 +19,49 @@ class ListView(View):
             category = GoodsCategory.objects.get(id=category_id)
         except GoodsCategory.DoesNotExist:
             return http.HttpResponseNotFound('GoodsCategory does not exist')
+        # 接收sort参数：如果用户不传，就是默认的排序规则
+        sort = request.GET.get('sort', 'default')
+
+        # 按照排序规则查询该分类商品SKU信息
+        if sort == 'price':
+            # 按照价格由低到高
+            sort_field = 'price'
+        elif sort == 'hot':
+            # 按照销量由高到低
+            sort_field = '-sales'
+        else:
+            # 'price'和'sales'以外的所有排序方式都归为'default'
+            sort = 'default'
+            sort_field = 'create_time'
 
         # 查询商品频道分类
         categories = get_categories()
         # 查询面包屑导航
         breadcrumb = get_breadcrumb(category)
 
+        skus = category.sku_set.filter(is_launched=True).order_by(sort_field)
+        # skus = SKU.objects.filter(category_id=category_id, is_launched=True)
+        # skus = SKU.objects.filter(category=category, is_launched=True).order_by(sort_field)
+
+        # 创建分页器：每页N条记录
+        paginator = Paginator(skus, constants.GOODS_LIST_LIMIT)
+        # 获取每页商品数据
+        try:
+            page_skus = paginator.page(page_num)
+        except EmptyPage:
+            # 如果page_num不正确，默认给用户404
+            return http.HttpResponseNotFound('empty page')
+        # 获取列表页总页数
+        total_page = paginator.num_pages
+
         # 渲染页面
         context = {
-            'categories':categories,
-            'breadcrumb': breadcrumb
+            'categories': categories,   # 频道分类
+            'breadcrumb': breadcrumb,   # 面包屑导航
+            'sort': sort,               # 排序字段
+            'category': category,       # 第三级分类
+            'page_skus': page_skus,     # 分页后数据
+            'total_page': total_page,   # 总页数
+            'page_num': page_num,       # 当前页码
         }
         return render(request, 'list.html', context)

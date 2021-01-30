d025eba04547e40c769eecae87eecdffcaff8e21
devensh <devensh@163.com>
2021-01-30 17:30:20 +0800

    商品-商品详情-展示详情页数据

--- meiduo_mall/meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/meiduo_mall/apps/goods/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', views.ListView.as_view(), name='list'),  # 商品列表页
     url(r'^hot/(?P<category_id>\d+)/$', views.HotGoodsView.as_view()),  # 热销商品排行
-    url(r'^detail/(?P<sku_id>\d+)/$', views.DetailView.as_view()),  # 商品详情
+    url(r'^detail/(?P<sku_id>\d+)/$', views.DetailView.as_view(), name='detail'),  # 商品详情
 ]

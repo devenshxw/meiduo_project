3ea0cbdf2992d1961ada94b250f0810e5176c47e
devensh <devensh@163.com>
2021-01-30 19:26:52 +0800

    商品-用户浏览记录

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     url(r'^addresses/(?P<address_id>\d+)/$', views.UpdateDestroyAddressView.as_view()),  # 更新和删除收货地址
     url(r'^addresses/(?P<address_id>\d+)/default/$', views.DefaultAddressView.as_view()),  # 设置默认地址
     url(r'^addresses/(?P<address_id>\d+)/title/$', views.UpdateTitleAddressView.as_view()),  # 修改地址标题
+    url(r'^browse_histories/$', views.UserBrowseHistory.as_view()),  # 用户浏览记录
 ]

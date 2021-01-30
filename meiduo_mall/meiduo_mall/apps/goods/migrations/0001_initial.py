769cc305d2f8de8426290dbc6b86086d0f24ebe3
devensh <devensh@163.com>
2021-01-30 18:40:25 +0800

    商品-商品详情-统计分类商品访问量

--- meiduo_mall/meiduo_mall/apps/goods/migrations/0001_initial.py
+++ meiduo_mall/meiduo_mall/apps/goods/migrations/0001_initial.py
@@ -90,7 +90,7 @@ class Migration(migrations.Migration):
                 ('sales', models.IntegerField(default=0, verbose_name='销量')),
                 ('comments', models.IntegerField(default=0, verbose_name='评价数')),
                 ('is_launched', models.BooleanField(default=True, verbose_name='是否上架销售')),
-                ('default_image_url', models.CharField(blank=True, default='', max_length=200, null=True, verbose_name='默认图片')),
+                ('default_image', models.ImageField(blank=True, default='', max_length=200, null=True, verbose_name='默认图片')),
                 ('category', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, to='goods.GoodsCategory', verbose_name='从属类别')),
             ],
             options={
--- meiduo_mall/meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/meiduo_mall/apps/goods/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', views.ListView.as_view(), name='list'),  # 商品列表页
     url(r'^hot/(?P<category_id>\d+)/$', views.HotGoodsView.as_view()),  # 热销商品排行
     url(r'^detail/(?P<sku_id>\d+)/$', views.DetailView.as_view(), name='detail'),  # 商品详情
+    url(r'^detail/visit/(?P<category_id>\d+)/$', views.DetailVisitView.as_view()),  # 统计分类商品访问量
 ]

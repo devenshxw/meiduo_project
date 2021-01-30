de5eaf6791c4735a3bbd9b4b48bb13369eb36591
devensh <devensh@163.com>
2021-01-30 17:00:56 +0800

    商品-商品详情-商品详情页分析和准备

--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -87,3 +87,28 @@ class HotGoodsView(View):
             })
 
         return http.JsonResponse({'code':RETCODE.OK, 'errmsg':'OK', 'hot_skus':hot_skus})
+
+
+class DetailView(View):
+    """商品详情页"""
+
+    def get(self, request, sku_id):
+        """提供商品详情页"""
+        # 获取当前sku的信息
+        try:
+            sku = SKU.objects.get(id=sku_id)
+        except SKU.DoesNotExist:
+            return render(request, '404.html')
+
+        # 查询商品频道分类
+        categories = get_categories()
+        # 查询面包屑导航
+        breadcrumb = get_breadcrumb(sku.category)
+
+        # 渲染页面
+        context = {
+            'categories': categories,
+            'breadcrumb':  breadcrumb,
+            'sku': sku,
+        }
+        return render(request, 'detail.html', context)

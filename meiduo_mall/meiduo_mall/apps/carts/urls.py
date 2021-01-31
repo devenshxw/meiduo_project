2cdff17d29ac242622568a4a49de0032221a43c5
devensh <devensh@163.com>
2021-01-31 21:57:57 +0800

    购物车-购物车管理-全选购物车

--- meiduo_mall/meiduo_mall/apps/carts/urls.py
+++ meiduo_mall/meiduo_mall/apps/carts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^carts/$', views.CartsView.as_view(), name='info'),  # 购物车
+    url(r'^carts/selection/$', views.CartsSelectAllView.as_view()),  # 全选购物车
 ]
--- meiduo_mall/meiduo_mall/apps/carts/views.py
+++ meiduo_mall/meiduo_mall/apps/carts/views.py
@@ -258,3 +258,43 @@ class CartsView(View):
                 response.set_cookie('carts', cookie_cart_str, max_age=constants.CARTS_COOKIE_EXPIRES)
             return response
 
+
+class CartsSelectAllView(View):
+    """全选购物车"""
+
+    def put(self, request):
+        # 接收参数
+        json_dict = json.loads(request.body.decode())
+        selected = json_dict.get('selected', True)
+
+        # 校验参数
+        if selected:
+            if not isinstance(selected, bool):
+                return http.HttpResponseForbidden('参数selected有误')
+
+        # 判断用户是否登录
+        user = request.user
+        if user is not None and user.is_authenticated:
+            # 用户已登录，操作redis购物车
+            redis_conn = get_redis_connection('carts')
+            cart = redis_conn.hgetall('carts_%s' % user.id)
+            sku_id_list = cart.keys()
+            if selected:
+                # 全选
+                redis_conn.sadd('selected_%s' % user.id, *sku_id_list)
+            else:
+                # 取消全选
+                redis_conn.srem('selected_%s' % user.id, *sku_id_list)
+            return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '全选购物车成功'})
+        else:
+            # 用户未登录，操作cookie购物车
+            cart = request.COOKIES.get('carts')
+            response = http.JsonResponse({'code': RETCODE.OK, 'errmsg': '全选购物车成功'})
+            if cart is not None:
+                cart = pickle.loads(base64.b64decode(cart.encode()))
+                for sku_id in cart:
+                    cart[sku_id]['selected'] = selected
+                cookie_cart = base64.b64encode(pickle.dumps(cart)).decode()
+                response.set_cookie('carts', cookie_cart, max_age=constants.CARTS_COOKIE_EXPIRES)
+
+            return response

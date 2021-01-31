eddedd9c19f6853164278c359629be92ae3409be
devensh <devensh@163.com>
2021-01-31 22:28:49 +0800

    购物车-展示商品页面简单购物车

--- meiduo_mall/meiduo_mall/apps/carts/urls.py
+++ meiduo_mall/meiduo_mall/apps/carts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^carts/$', views.CartsView.as_view(), name='info'),  # 购物车
     url(r'^carts/selection/$', views.CartsSelectAllView.as_view()),  # 全选购物车
+    url(r'^carts/simple/$', views.CartsSimpleView.as_view()),  # 简单购物车数据
 ]

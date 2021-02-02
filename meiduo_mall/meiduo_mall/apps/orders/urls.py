1b2d0aac393da9bde7693ad44739c8748380b6ef
devensh <devensh@163.com>
2021-02-02 20:08:59 +0800

    订单-提交订单-保存订单基本信息和订单商品信息

--- meiduo_mall/meiduo_mall/apps/orders/urls.py
+++ meiduo_mall/meiduo_mall/apps/orders/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^orders/settlement/$', views.OrderSettlementView.as_view(), name="settlement"),  # 结算订单
+    url(r'^orders/commit/$', views.OrderCommitView.as_view(), name="commit"),  # 提交订单
 ]
